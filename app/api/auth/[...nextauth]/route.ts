8751f3c9f396db5d77f95afdec1c8632d4a96f73
施宏锦 <543528285@qq.com>
2023-07-30 12:08:02 +0800

    feat: 配置jwt信息

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,10 @@ import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 
 const handler = NextAuth({
+    session: { strategy: 'jwt' },
+    jwt: {
+        secret: 'test',
+    },
     providers: [
         GithubProvider({
             // @ts-ignore
@@ -11,6 +15,21 @@ const handler = NextAuth({
         }),
         // ...add more providers here
     ],
+    callbacks: {
+        async jwt({ token, account }) {
+            // Persist the OAuth access_token to the token right after signin
+            if (account) {
+                token.accessToken = account.access_token
+            }
+            return token
+        },
+        async session({ session, token, user }) {
+            // Send properties to the client, like an access_token from a provider.
+            // @ts-ignore
+            session.accessToken = token.accessToken
+            return session
+        },
+    },
 })
 
 export { handler as GET, handler as POST }
--- app/components/LoginButton.tsx
+++ app/components/LoginButton.tsx
@@ -2,10 +2,12 @@ import { useSession, signIn, signOut } from 'next-auth/react'
 
 export default function LoginButton() {
     const { data: session }: any = useSession()
+
     if (session) {
         return (
             <>
                 Signed in as {session.user.email} <br />
+                {session.accessToken}
                 <button onClick={() => signOut()}>Sign out</button>
             </>
         )
