009944daecf665ad62e79fc6a372e94abbaeea64
施宏锦 <543528285@qq.com>
2023-08-01 09:14:40 +0800

    feat: 连接数据库

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,16 @@
-import NextAuth from 'next-auth'
+import NextAuth, { AuthOptions } from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { PrismaClient } from '@prisma/client'
 
-const handler = NextAuth({
+const prisma = new PrismaClient()
+
+export const authOptions: AuthOptions = {
+    // @ts-ignore
+    adapter: PrismaAdapter(prisma),
     session: { strategy: 'jwt' },
     jwt: {
-        secret: 'test',
+        maxAge: 60 * 60 * 24 * 30,
     },
     providers: [
         GithubProvider({
@@ -12,6 +18,9 @@ const handler = NextAuth({
             clientId: process.env.GITHUB_ID,
             // @ts-ignore
             clientSecret: process.env.GITHUB_SECRET,
+            httpOptions: {
+                timeout: 50000,
+            },
         }),
         // ...add more providers here
     ],
@@ -27,9 +36,17 @@ const handler = NextAuth({
             // Send properties to the client, like an access_token from a provider.
             // @ts-ignore
             session.accessToken = token.accessToken
+            if (user) {
+                session.user.id = user.id
+            } else {
+                // @ts-ignore
+                session.user.id = token.sub
+            }
             return session
         },
     },
-})
+}
+
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
