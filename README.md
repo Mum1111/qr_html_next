7f951b4447234c6729b5e4503eda31be66c3f044
施宏锦 <543528285@qq.com>
2023-08-10 18:34:43 +0800

    feat: logo url落盘

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# next13空白模版
+# 二维码生成网站
 
 ## 启动项目
 
@@ -18,6 +18,20 @@ yarn dev
 # or
 pnpm dev
 ```
+
+创建数据库表
+```bash
+npx  prisma migrate dev  
+```
+
+## TODO
+
+1. logo表设计  完成
+2. logo上传oss后，oss地址落盘到数据库 完成
+3. 根据用户信息查询logo接口， 并在前端渲染
+4. mui全局主题调整，解决与tailwindcss的兼容性问题
+5. 样式优化
+6. 部署上线
 
 
 
--- app/api/common/upload/route.ts
+++ app/api/common/upload/route.ts
@@ -1,8 +1,10 @@
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 import { NextResponse } from 'next/server'
-import oss from 'ali-oss'
 import { upLoadToOss } from '@/app/api/common/upload/alioss'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 export async function POST(req: NextResponse) {
     // 获取当前用户id
@@ -24,10 +26,16 @@ export async function POST(req: NextResponse) {
 
     const { url, err } = await upLoadToOss(file)
 
-    console.log('url', url)
     if (err) return NextResponse.json({ error: '上传失败' }, { status: 500 })
     // 将阿里云返回的url 存储的logo_image db
 
+    const row = await prisma.logo.create({
+        data: {
+            imageUrl: url,
+            userId: session.user.id,
+        },
+    })
+
     // 返回前端成功信息
-    return NextResponse.json({ url }, { status: 200 })
+    return NextResponse.json({ success: '上传成功' }, { status: 200 })
 }
